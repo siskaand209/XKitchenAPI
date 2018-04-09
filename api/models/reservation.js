c13e815efaf57cdd3fe23424f8afb91dbabbdbd2
siskaand209 <siskakamilfb@gmail.com>
2018-04-09 14:12:09 +0700

    no.referensi

--- api/models/reservation.js
+++ api/models/reservation.js
@@ -1,16 +1,13 @@
-// tahap 1
- const express = require('express');
- const router = express.Router();
 const mongoose = require('mongoose');
 
 const reservationSchema = mongoose.Schema({
     _id : mongoose.Schema.Types.ObjectId,
-    table : {type: mongoose.Schema.Types.ObjectId, ref: 'Table', require: true},
-    user : {type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true},
-    reference : {type : String, require: true, trim: true},
-    guest : {type : String, require: true, trim: true},
-    paid: {type: Boolean, require: true, trim: true},
-    created: { type: Date, require: true, trim: true, default: Date.now()}
+    table : { type: mongoose.Schema.Types.ObjectId, ref: 'Table', required: true},
+    waiter : { type: mongoose.Schema.Types.ObjectId, ref: 'Waiter', required: true},
+    reference : { type: String, required: true, trim: true },
+    guest : { type: String, required: true, trim: true },
+    paid: { type: Boolean, required: true, default: false },
+    created : { type: Date, required: true, trim: true, default: Date.now() }
 });
 
-module.exports = mongoose.model('Reservation'/*ini dari const router*/, reservationSchema, 'reservations');
+module.exports = mongoose.model('Reservation', reservationSchema, 'reservations');
--- api/routers/reservations.js
+++ api/routers/reservations.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
+//User Model
 const Reservation = require('../models/reservation');
+const Table = require('../models/table');
 
-//Get All
+//Get all
 router.get('/', (req, res, next) => {
     Reservation.find()
-        .populate('table','code seat description')
-        .populate('user','badgeId nick fullName')
+        .populate('table', 'seat description')
+        .populate('waiter', 'userId nick fullName')
         .exec()
         .then(doc => {
             res.status(200).json(doc);
@@ -16,34 +18,141 @@ router.get('/', (req, res, next) => {
         .catch(err => {
             console.log(err);
             res.status(500).json({
-                error: err
+                error : err
             });
         });
-}); //end get
+});
+
+//Get all by table
+router.get('/table/:id', (req, res, next) => {
+    var id = req.params.id;
+    //res.status(200).json(req.params.id);
+    GetLatestTable(id, response => {
+        Table.findOne({_id: id})
+            .exec()
+            .then(doc => {
+                res.status(200).json({
+                    table: doc,
+                    reservation: response});
+            }).catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error : err
+                });
+            });
+    });
+});
+
+function GetLatestTable(tableId, callback){
+    Reservation.findOne({table: tableId})
+        .sort({reference: -1})
+        .exec((err, doc) => {
+            if (doc != null) {
+                return callback(doc);
+            } else {
+                return callback(null);
+            }
+        });
+}
 
 //Insert
-router.post('/',(req,res,next) => {
-    const newReservation = new Reservation({
-        _id : new mongoose.Types.ObjectId,
-        table : req.body.table,
-        user : req.body.user,
-        reference : req.body.reference,
-        guest : req.body.guest,
-       // createDate : req.body.createDate,
-        //Time
+router.post('/', (req, res, next) => {
+    //var newRef = "";
+    GetNewReference(response => {
+        //newRef = response;
+        const newReservation = new Reservation({
+            _id : new mongoose.Types.ObjectId(),
+            table : req.body.tableId,
+            waiter : req.body.waiterId,
+            reference : response,
+            guest : req.body.guest,
+            paid: req.body.paid
+        });
+    
+        newReservation.save()
+            .then(result => {
+                console.log(result);
+                res.status(201).json(result);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error : err
+                });
+            })
     });
+});
 
-    newReservation.save()
+function GetNewReference(callback){
+    var newRef = "SLS-" + new Date().getFullYear().toString().substr(-2) + ("0" + (new Date().getMonth() + 1)).slice(-2) + "-";
+    var lastRef = newRef + "0001";
+
+    Reservation.findOne({reference: new RegExp(newRef, 'i')})
+        .sort({reference: -1})
+        .exec((err, doc) => {
+        if (doc != null) {
+            var arr = doc.reference.split("-");
+            var inc = parseInt(arr[2]) + 1;
+            lastRef = newRef + ("0000" + inc).slice(-4);
+            return callback(lastRef);
+        } else {
+            return callback(lastRef);
+        }
+    });
+}
+
+
+//Get by (id)
+router.get('/:id', (req, res, next) => {
+    console.log("Return form GetNew : ");
+    GetNewReference(response => {
+        console.log(response);
+    });
+    const id = req.params.id;
+    Reservation.findById(id)
+        .populate('table', 'seat description')
+        .populate('waiter', 'userId nick fullName')
+        .exec()
         .then(result => {
             console.log(result);
-            res.status(201).json(result);
+            res.status(200).json(result);
         })
         .catch(err => {
             console.log(err);
             res.status(500).json({
-                error : err
-            });
+                error: err
+            })
         })
+});
+
+router.patch('/:id', (req, res, next) => {
+    const id = req.params.id;
+    Reservation.update({ _id : id }, { $set: req.body })
+        .exec()
+        .then( result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message : err
+            });
+        });
+});
+
+router.delete('/:id', (req, res, next) => {
+    const id = req.params.id;
+    Reservation.remove({ _id : id })
+        .exec()
+        .then( result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message : err
+            });
+        });
 });
 
 module.exports = router;
